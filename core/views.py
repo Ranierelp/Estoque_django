8b77cf7b9bfd5f13069834158c60d192c1d54b63
ranierelp <raierepereira21@outlook.com>
2024-02-26 11:14:51 -0300

    first commit

---
+++ core/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html')
+
+def contato(request):
+    return render(request, 'contato.html')
+
+def produto(request):
+    return render(request, 'produto.html')
